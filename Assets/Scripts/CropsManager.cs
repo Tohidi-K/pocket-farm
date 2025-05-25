656836535cd6627e98ea77554a41820fd8a6f5d4
Tohidi-K <ali.tohidi.karimii@gmail.com>
2025-05-25 17:38:24 -0600

    Implement crop upgrade system with UI and data handling
    - Added an upgrade button for each crop
    - Created a CropData script to store upgrade prices and profit values per level
    - Implemented upgrade logic for buttons to increase crop level and update values
    - Added a level display at the top-left corner of each crop to show current level

--- Assets/Scripts/CropsManager.cs
+++ Assets/Scripts/CropsManager.cs
@@ -1,17 +1,26 @@
-using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CropsManager : MonoBehaviour
 {
     public int gold = 30;
     public int[] cropPrices = new int[] { 50, 100, 150, 250, 400, 600, 850, 1150, 1500, 1900};
+    public int[] cropLevel = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     public int i = 0;
     public bool isMoneyEnough;
 
+    public List<GameObject> cropsButtons;
+    public List<GameObject> upgradeButtons;
+    public List<TMP_Text> upgradeText;
+
     private TextDisplayManager textDisplayManager;
+    private CropData cropData;
     void Awake()
     {
         textDisplayManager = GameObject.Find("UIManager").GetComponent<TextDisplayManager>();
+        cropData = GameObject.Find("CropsManager").GetComponent<CropData>();
     }
 
     public void BuyingNewCrop()
@@ -30,6 +39,40 @@ public class CropsManager : MonoBehaviour
         else
         {
             isMoneyEnough = false;
+        }
+    }
+
+    public void ActivateUpgradeButton(int cropIndex)
+    {
+
+        if (!cropsButtons[cropIndex].activeInHierarchy)
+        {
+            if (gold < cropData.upgradePrices[cropIndex, 0])
+            {
+                upgradeButtons[cropIndex].GetComponent<Image>().color = new Color(0.77f, 0.77f, 0.77f);
+                upgradeText[cropIndex].color = new Color(096f, 0.34f, 0.34f);
+            }
+            else
+            {
+                upgradeButtons[cropIndex].GetComponent<Image>().color = new Color(0.75f, 1, 0);
+                upgradeText[cropIndex].color = new Color(1, 1, 1);
+            }
+            upgradeText[cropIndex].text = cropData.upgradePrices[cropIndex, 0].ToString();
+            upgradeButtons[cropIndex].SetActive(true);
+            textDisplayManager.UpdateLevel(cropIndex);
+            textDisplayManager.cropLevel[cropIndex].gameObject.SetActive(true);
+        }
+    }
+
+    public void UpgradeCrop(int cropIndex)
+    {
+        if (gold >= cropData.upgradePrices[cropIndex, cropLevel[cropIndex]])
+        {
+            gold -= cropData.upgradePrices[cropIndex, cropLevel[cropIndex]];
+            textDisplayManager.UpdateCoin();
+            cropLevel[cropIndex]++;
+            upgradeText[cropIndex].text = cropData.upgradePrices[cropIndex, cropLevel[cropIndex]].ToString();
+            textDisplayManager.UpdateLevel(cropIndex);
         }
     }
 }
